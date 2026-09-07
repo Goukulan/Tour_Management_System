06406604a2441d83ce8c79a3b8da4f6673fcd870
Goukulan <goukulan.s@gmail.com>
2026-09-07 11:16:43 +0530

    Changed directory

--- app/api/v1/api_v1.py
+++ app/api/v1/api_v1.py
@@ -1,18 +1,18 @@
 from fastapi import APIRouter
 
 from app.api.v1.endpoints.auth import router as auth_router
-from app.api.v1.endpoints.admin.destinations import router as destination_router
-from app.api.v1.endpoints.admin.categories import router as categories_router
-from app.api.v1.endpoints.admin.tours import router as admin_tours_router
-from app.api.v1.endpoints.admin.departures import router as admin_departures_router
-from app.api.v1.endpoints.admin.guides import router as guide_router
-from app.api.v1.endpoints.admin.users import router as users_router
+from app.api.v1.endpoints.admin_destinations import router as destination_router
+from app.api.v1.endpoints.admin_categories import router as categories_router
+from app.api.v1.endpoints.admin_tours import router as admin_tours_router
+from app.api.v1.endpoints.admin_departures import router as admin_departures_router
+from app.api.v1.endpoints.admin_guides import router as guide_router
+from app.api.v1.endpoints.admin_users import router as users_router
 from app.api.v1.endpoints.destinations import router as public_destinations_router
 from app.api.v1.endpoints.categories import router as public_categories_router
 from app.api.v1.endpoints.tours import router as public_tours_router
 from app.api.v1.endpoints.bookings import router as bookings
-from app.api.v1.endpoints.admin.bookings import router as temporary_booking
-from app.api.v1.endpoints.guide.departures import router as guide_departures_router
+from app.api.v1.endpoints.admin_bookings import router as temporary_booking
+from app.api.v1.endpoints.guide_departures import router as guide_departures_router
 from app.api.v1.endpoints.payments import router as payments_router
 
 
